dca007cfbe8cd905f1dfe20a98499a3fd7267941
eborweed <ibrahimwaheed8@gmail.com>
2025-08-02 19:35:30 +1200

    stupid mistake

--- src/components/MemoryMap.jsx
+++ src/components/MemoryMap.jsx
@@ -1,5 +1,4 @@
 import React, { useRef, useEffect, useState } from 'react';
-import React, { useRef, useEffect, useState } from 'react';
 import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import './MemoryMap.css';
