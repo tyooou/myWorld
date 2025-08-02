1514b1e68542033003955d6f4e99ede4e730a415
Oshan Premkumar <opre469@aucklanduni.ac.nz>
2025-08-02 16:22:51 +1200

    resolved conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,20 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import MemoryMap from "./components/MemoryMap";
+
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import MemoryMap from './components/MemoryMap'
-import LoadingPage from "./LoadingScreens/LoadingPage.jsx";
-import LoadingPage2 from "./LoadingScreens/LoadingPage2.jsx";
 import MainLoader from "./LoadingScreens/MainLoader.jsx";
-function App() {
+
 
 function App() {
   return (
     <>
-      <MainLoader/>
+    <Header />
+     <MemoryMap />
+    {/* <MainLoader /> */}
+      <Footer />
     </>
   );
 }
